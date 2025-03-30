b9f13482cd9888713ab13075426b84d7e7922d26
igorgonribs <igorgonribsilva@gmail.com>
2025-03-30 14:59:19 -0300

    test dependencies

---
+++ car-insurance-api/src/main/test/com/car/insurance/api/domain/service/impl/CarServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.car.insurance.api.domain.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarServiceImplTest {
+
+    @DisplayName("Single test successful")
+    @Test
+    void testSingleSuccessTest() {
+        System.out.println("Test is running");
+    }
+
+}
